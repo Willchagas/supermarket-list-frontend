da315e7cbba862b0b366cb843051df64603e3a20
Willchagas <williampch49@gmail.com>
2023-01-06 10:33:19 -0300

    feat: eslint, prettier and absolute imports

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,71 +1,71 @@
-import { useEffect, useState } from "react";
-import "./index.css";
-import { Input } from "../Input";
-import { Button } from "../Button";
-import { createItem, updateItem, deleteItem } from "../../services/request";
+import { useEffect, useState } from 'react'
+import './index.css'
+import { Input } from 'components/Input'
+import { Button } from 'components/Button'
+import { createItem, updateItem, deleteItem } from 'services/request'
 
 export const Modal = ({ onClose, item }) => {
-  const [name, setName] = useState("");
-  const [quantity, setQuantity] = useState(1);
+  const [name, setName] = useState('')
+  const [quantity, setQuantity] = useState(1)
 
   const validateBeforeSave = () => {
     if (name.length < 3) {
-      alert("Nome tem que ter mais de 3 caracteres");
-      return;
+      alert('Nome tem que ter mais de 3 caracteres')
+      return
     }
     if (quantity < 1) {
-      alert("Quantidade não pode ser menor do que 1");
-      return;
+      alert('Quantidade não pode ser menor do que 1')
+      return
     }
-    return true;
-  };
+    return true
+  }
 
   const callAddItem = async () => {
-    const validate = validateBeforeSave();
+    const validate = validateBeforeSave()
     if (validate) {
-      const result = await createItem({ name, quantity: Number(quantity) });
+      const result = await createItem({ name, quantity: Number(quantity) })
       if (!result?.error) {
-        alert("Item salvo com sucesso");
-        onClose();
+        alert('Item salvo com sucesso')
+        onClose()
       }
     }
-  };
+  }
 
   const callUpdateItem = async () => {
-    const validate = validateBeforeSave();
+    const validate = validateBeforeSave()
     if (validate) {
       const result = await updateItem(item?._id, {
         name,
         quantity: Number(quantity),
-        checked: item?.checked,
-      });
+        checked: item?.checked
+      })
       if (!result?.error) {
-        alert("Item atualizado com sucesso");
-        onClose();
+        alert('Item atualizado com sucesso')
+        onClose()
       }
     }
-  };
+  }
 
   const callDeleteItem = async () => {
-    const result = await deleteItem(item?._id);
+    const result = await deleteItem(item?._id)
     if (!result?.error) {
-      alert("Item deletado com sucesso");
-      onClose();
+      alert('Item deletado com sucesso')
+      onClose()
     }
-  };
+  }
 
   useEffect(() => {
     if (item?.name && item?.quantity) {
-      setName(item?.name);
-      setQuantity(item?.quantity);
+      setName(item?.name)
+      setQuantity(item?.quantity)
     }
-  }, [item]);
+  }, [item])
 
   return (
     <div className="modal">
       <div className="modal-content">
         <div className="modal-header">
-          <h1>{item ? "Editar item" : "Adicionar novo item"}</h1>
+          <h1>{item ? 'Editar item' : 'Adicionar novo item'}</h1>
           <button onClick={onClose} className="modal-close-button" />
         </div>
         <Input
@@ -82,7 +82,7 @@ export const Modal = ({ onClose, item }) => {
           type="number"
         />
         <Button onClick={item ? callUpdateItem : callAddItem}>
-          {item ? "Atualizar" : "Adicionar"}
+          {item ? 'Atualizar' : 'Adicionar'}
         </Button>
         {item && (
           <Button icon="trash" variant="outline" onClick={callDeleteItem}>
@@ -91,5 +91,5 @@ export const Modal = ({ onClose, item }) => {
         )}
       </div>
     </div>
-  );
-};
+  )
+}
--- src/services/request/index.js
+++ src/services/request/index.js
@@ -1,45 +1,45 @@
-import { api } from "../api";
+import { api } from 'services/api'
 
 export const getList = async () => {
   try {
-    const result = await api.get("/list-items");
-    return result.data;
+    const result = await api.get('/list-items')
+    return result.data
   } catch (error) {
-    console.log("Erro ao buscar dados da API");
-    return { error };
+    console.log('Erro ao buscar dados da API')
+    return { error }
   }
-};
+}
 
 export const createItem = async (item) => {
   try {
-    const result = await api.post("/list-item", {
-      ...item,
-    });
-    return result.data;
+    const result = await api.post('/list-item', {
+      ...item
+    })
+    return result.data
   } catch (error) {
-    alert("Erro ao salvar novo item");
-    return { error };
+    alert('Erro ao salvar novo item')
+    return { error }
   }
-};
+}
 
 export const updateItem = async (id, item) => {
   try {
     const result = await api.put(`/list-item/${id}`, {
-      ...item,
-    });
-    return result.data;
+      ...item
+    })
+    return result.data
   } catch (error) {
-    alert("Erro ao atualizar novo item");
-    return { error };
+    alert('Erro ao atualizar novo item')
+    return { error }
   }
-};
+}
 
 export const deleteItem = async (id) => {
   try {
-    const result = await api.delete(`/list-item/${id}`);
-    return result.data;
+    const result = await api.delete(`/list-item/${id}`)
+    return result.data
   } catch (error) {
-    alert("Erro ao deleta item");
-    return { error };
+    alert('Erro ao deleta item')
+    return { error }
   }
-};
+}
